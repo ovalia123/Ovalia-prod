205acc1d8346f19b5231f4660f164119c8c5b742
ovalia123 <mohahnfrance@gmail.com>
2026-01-18 11:33:38 -0500

    Update urls.py

--- ovalia2/urls.py
+++ ovalia2/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("myapp.urls")),
     path("gestion/", include("gestion.urls")),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
